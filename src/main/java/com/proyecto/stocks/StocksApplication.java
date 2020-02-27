d80a40107dd559099007987d66ae72d33042b5a2
CarlosVilasAlvarez <carlosvilasalvarez@gmail.com>
2020-02-27 19:47:49 +0100

    Commit inicial

---
+++ src/main/java/com/proyecto/stocks/StocksApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.stocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyecto/stocks/StocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.stocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
