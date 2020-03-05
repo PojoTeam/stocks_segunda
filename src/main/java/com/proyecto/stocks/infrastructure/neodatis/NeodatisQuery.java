99f3b1f43435cb20aadfc80c9fa049f7e1c53006
Pablo <pablobarroscordo@gmail.com>
2020-03-05 13:44:58 +0100

    Neodatis one query, not implemented

--- src/main/java/com/proyecto/stocks/infrastructure/neodatis/NeodatisQuery.java
+++ src/main/java/com/proyecto/stocks/infrastructure/neodatis/NeodatisQuery.java
@@ -1,4 +1,21 @@
 package com.proyecto.stocks.infrastructure.neodatis;
 
+import com.proyecto.stocks.model.Company;
+import org.neodatis.odb.ODB;
+import org.neodatis.odb.Objects;
+
+import java.util.ArrayList;
+
 public class NeodatisQuery {
+    public static ArrayList<Company> companies(ODB odb) {
+        Company company = null;
+        ArrayList<Company> companies = null;
+        Objects<Company> objects = odb.getObjects(Company.class);
+
+        while (objects.hasNext()) {
+            company = objects.next();
+            companies.add(company);
+        }
+        return companies;
+    }
 }
